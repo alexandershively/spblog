2694f565da7ba65e1fb1097e11c98a62a4d18b1d
alexandershively <alexandershively@gmail.com>
2016-01-28 18:15:29 -0800

    heroku deploy 1.28

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/destroy'
+
   get 'pages/home'
 
   get 'pages/about'
@@ -9,6 +13,7 @@ Rails.application.routes.draw do
     resources :comments
   end
   resources :users
+  resources :sessions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160128191343) do
+ActiveRecord::Schema.define(version: 20160128234901) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -33,12 +33,14 @@ ActiveRecord::Schema.define(version: 20160128191343) do
   add_index "comments", ["article_id"], name: "index_comments_on_article_id", using: :btree
 
   create_table "users", force: :cascade do |t|
-    t.string   "password",   limit: 255
-    t.string   "email",      limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
-    t.string   "username",   limit: 255
-    t.integer  "posts",      limit: 4
+    t.string   "password",           limit: 255
+    t.string   "email",              limit: 255
+    t.datetime "created_at",                     null: false
+    t.datetime "updated_at",                     null: false
+    t.string   "username",           limit: 255
+    t.integer  "posts",              limit: 4
+    t.string   "encrypted_password", limit: 255
+    t.string   "salt",               limit: 255
   end
 
   add_foreign_key "comments", "articles"
